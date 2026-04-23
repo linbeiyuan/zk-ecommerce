7ce4e0343ddf513b649b3bc1dc521547087e1906
林北元 <linbeiyuan@example.com>
2026-04-23 19:46:58 +0800

    1

--- front/front/src/layout/footer.vue
+++ front/front/src/layout/footer.vue
@@ -2,7 +2,7 @@
   <footer class="bg-gray-900 text-white py-12">
     <div class="max-w-7xl mx-auto px-4">
       <div class="pt-8 text-center text-gray-400">
-        <p>&copy; 2025 XXXXX 版权所有</p>
+        <p>&copy; 2026 XXXXX 版权所有</p>
       </div>
     </div>
   </footer>
